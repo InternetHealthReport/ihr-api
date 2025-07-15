8ac67e5cd939e6e3fdeb97c21663bd5212fffd45
Dimitrios Giakatos <dimitrios@iij.ad.jp>
2025-07-15 13:47:05 +0900

    add documentation to /docs

--- main.py
+++ main.py
@@ -3,8 +3,26 @@ import pkgutil
 from fastapi import FastAPI
 from controllers import __path__ as controllers_path
 
+description = """
+```[ Base URL: www.ihr.live/ihr/api ]```
+
+This RESTful API is intended for developers and researchers integrating IHR data to their workflow. API data is also available via our [Python library](https://www.ihr.live/ihr/en-us/documentation#Python_Library).
+
+**For bulk downloads please use: [https://ihr-archive.iijlab.net/](https://ihr-archive.iijlab.net/)**
+
+Parameters ending with __lte and __gte (acronyms for 'less than or equal to', and, 'greater than or equal to') are used for selecting a range of values.
+
+[Contact the developers](mailto:admin@ihr.live)
+
+[Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)](https://creativecommons.org/licenses/by-nc-sa/4.0/)
+"""
+
 # The base URL of the app
-app = FastAPI()
+app = FastAPI(
+    title="IHR API",
+    description=description,
+    version="0.1.0"
+)
 
 # Automatically import and register all routers inside "controllers"
 for _, module_name, _ in pkgutil.iter_modules(controllers_path):
