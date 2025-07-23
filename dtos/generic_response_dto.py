9fc67e5749f1e5dda8f5cdbba56901fb4c4fed3c
Dimitrios Giakatos <dimitrios@iij.ad.jp>
2025-07-23 16:37:47 +0900

    fix proxy config

--- dtos/generic_response_dto.py
+++ dtos/generic_response_dto.py
@@ -2,6 +2,15 @@ from typing import TypeVar, List, Optional, Generic
 from fastapi import Request
 from urllib.parse import urlencode, urlunparse
 from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+
+try:
+    load_dotenv()
+except:
+    pass
+
+PROXY_PATH = os.getenv("PROXY_PATH")
 
 # T is a generic type variable that will be replaced with a specific DTO type (e.g., CountryDTO) 
 # when GenericResponseDTO is used (e.g GenericResponseDTO[CountryDTO] used in CountryController).
@@ -22,7 +31,7 @@ def build_url(request: Request, page: Optional[int]) -> Optional[str]:
     query_params["page"] = str(page)
     return urlunparse((
         request.url.scheme,
-        request.url.netloc,
+        request.url.netloc if PROXY_PATH is None else f"{request.url.netloc}/{PROXY_PATH}",
         request.url.path,
         "",
         urlencode(query_params),
