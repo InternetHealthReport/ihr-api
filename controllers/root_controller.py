369ec267bbceb43f3287810d09aef9b59723d59c
Dimitrios Giakatos <dimitrios@iij.ad.jp>
2025-08-26 11:20:12 +0900

    add custom swagger controller

--- controllers/root_controller.py
+++ controllers/root_controller.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from fastapi.responses import RedirectResponse
+from fastapi.openapi.docs import get_swagger_ui_html
 from dotenv import load_dotenv
 import os
 
@@ -15,5 +16,17 @@ router = APIRouter(prefix="", tags=["Root"])
 class RootController:
     @staticmethod
     @router.get("/", include_in_schema=False, redirect_slashes=True)
-    def get_all_countries():
+    def get_root():
         return RedirectResponse(url="/docs" if PROXY_PATH is None else f"/{PROXY_PATH}/docs")
+    
+    @staticmethod
+    @router.get("/docs", include_in_schema=False)
+    @router.get("/docs/", include_in_schema=False)
+    def get_swagger_ui_html():
+        return get_swagger_ui_html(
+            openapi_url=router.openapi_url,
+            title=router.title,
+            swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
+            swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
+        )
+
